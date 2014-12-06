e91faf7645dab6335dc1c8e25d49e98b582cab1b
Will Bradley <williambbradley@gmail.com>
2014-12-05 19:24:10 -0800

    complex mag

--- clojure/src/mandel/core.clj
+++ clojure/src/mandel/core.clj
@@ -2,14 +2,19 @@
   (:require [clojure.math.numeric-tower :as math])
   (:gen-class))
 
-(defn make-complex [real imag]
+(defn complex-make [real imag]
   {:real real :imag imag})
 
-(defn multiply-complex [c1 c2]
+(defn complex-multiply [c1 c2]
   (let [x (:real c1)
         y (:imag c1)
         u (:real c2)
         v (:imag c2)]
     {:real (- (* x u) (* y v))
      :imag (+ (* x v) (* y u))}))
+
+(defn complex-magnitude [c]
+  (math/sqrt (+
+              (* (:real c) (:real c)) 
+              (* (:imag c) (:imag c)))))
 
